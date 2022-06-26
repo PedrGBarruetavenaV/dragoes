a6a4cfda4e7122bd157e65776f7b42d1ea5d537a
PedrGBarruetavenaV <107200364+PedrGBarruetavenaV@users.noreply.github.com>
2022-06-26 10:33:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-// Por enquanto o javascript não funciona (tentei mas não deu)
 
-let botaoEl = document.querySelector('#abre-menu');
+let dragoesNav = document.querySelector('#abre-menu');
 let menuB = document.querySelector('#nav-dragoes');
+let conj = document.querySelectorAll('.conjunto-menu');
 
 function alternaMenu() {
-    menuB.classList.add('.visivel');
-    menuB.classList.remove('.invisivel');
+    menuB.classList.toggle('visivel');
+    menuB.classList.toggle('invisivel');
 }
 
-botaoEl.addEventListener('click', alternaMenu);
+dragoesNav.addEventListener('mouseover', alternaMenu);
+conj.addEventListener('mouseout', alternaMenu);
+
