0f9f20baedf830e578ad88257cc561bf6dd5b565
PedrGBarruetavenaV <pbarruetavenavieira@gmail.com>
2022-06-26 10:30:11 -0300

    melhorei o js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-let botaoEl = document.querySelector('#abre-menu');
+let dragoesNav = document.querySelector('#abre-menu');
 let menuB = document.querySelector('#nav-dragoes');
+let conj = document.querySelectorAll('.conjunto-menu');
 
 function alternaMenu() {
     menuB.classList.toggle('visivel');
     menuB.classList.toggle('invisivel');
 }
 
-botaoEl.addEventListener('click', alternaMenu);
+dragoesNav.addEventListener('mouseover', alternaMenu);
+conj.addEventListener('mouseout', alternaMenu);
