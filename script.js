e65705984d6dc21cf7505b7c470944e4f566dbc3
PedrGBarruetavenaV <pbarruetavenavieira@gmail.com>
2022-07-14 20:33:33 -0300

    botao

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let dragoesNav = document.querySelector('#abre-menu');
 let menuB = document.querySelector('#nav-dragoes');
 let conj = document.querySelectorAll('.conjunto-menu');
+let botaoMenu = document.querySelector('#botao-menu');
 
 function alternaMenu() {
     menuB.classList.toggle('visivel');
     menuB.classList.toggle('invisivel');
 }
 
-dragoesNav.addEventListener('mouseover', alternaMenu);
-conj.addEventListener('mouseout', alternaMenu);
+botaoMenu.addEventListener('click', alternaMenu);
+//dragoesNav.addEventListener('mouseover', alternaMenu);
+//conj.addEventListener('mouseout', alternaMenu);
