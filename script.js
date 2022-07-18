506a0f8116d0bd468b28812b5f0ce4780cc3272d
PedrGBarruetavenaV <pbarruetavenavieira@gmail.com>
2022-07-18 17:34:37 -0300

    update

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-let dragoesNav = document.querySelector('#abre-menu');
 let menuB = document.querySelector('#nav-dragoes');
-let conj = document.querySelectorAll('.conjunto-menu');
+
+//itens do menu para deslocar
+let menuDesloc1 = document.querySelector('#normal1');
+let menuDesloc2 = document.querySelector('#normal2');
+let menuDesloc3 = document.querySelector('#normal3');
+let menuDesloc4 = document.querySelector('#normal4');
+
 let botaoMenu = document.querySelector('#botao-menu');
 
 function alternaMenu() {
     menuB.classList.toggle('visivel');
     menuB.classList.toggle('invisivel');
+
+    //menu
+    menuB.classList.toggle('visivel-deslocado');
+    menuDesloc1.classList.toggle('desloc');
+    menuDesloc2.classList.toggle('desloc');
+    menuDesloc3.classList.toggle('desloc');
+    menuDesloc4.classList.toggle('desloc');
 }
 
 botaoMenu.addEventListener('click', alternaMenu);
 //dragoesNav.addEventListener('mouseover', alternaMenu);
-//conj.addEventListener('mouseout', alternaMenu);
