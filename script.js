0ad6c763f5cc156ba31931ee7910b898d9962d4b
PedrGBarruetavenaV <pbarruetavenavieira@gmail.com>
2022-07-19 15:54:37 -0300

    easter egg

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-let menuB = document.querySelector('#nav-dragoes');
+const menuB = document.querySelector('#nav-dragoes');
 
 //itens do menu para deslocar
-let menuDesloc1 = document.querySelector('#normal1');
-let menuDesloc2 = document.querySelector('#normal2');
-let menuDesloc3 = document.querySelector('#normal3');
-let menuDesloc4 = document.querySelector('#normal4');
+const menuDesloc1 = document.querySelector('#normal1');
+const menuDesloc2 = document.querySelector('#normal2');
+const menuDesloc3 = document.querySelector('#normal3');
+const menuDesloc4 = document.querySelector('#normal4');
 
-let botaoMenu = document.querySelector('#botao-menu');
+const botaoMenu = document.querySelector('#botao-menu');
+
 
 function alternaMenu() {
     menuB.classList.toggle('visivel');
